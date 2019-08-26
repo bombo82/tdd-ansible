754323f6b86d8772817715298c68e8b216f60b53
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2019-08-27 00:19:46 +0200

    test sos report package is installed

--- sos-report-galaxy/molecule/default/tests/test_default.py
+++ sos-report-galaxy/molecule/default/tests/test_default.py
@@ -7,9 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_sos_report_is_installed(host):
+    assert host.package('sos').is_installed
