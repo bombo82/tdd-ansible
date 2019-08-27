d998c4cf0bf941872c8727f6dd6c4b6336deef57
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2019-08-27 11:19:31 +0200

    implement test and automation distribution based

--- sos-report-molecule/molecule/default/tests/test_default.py
+++ sos-report-molecule/molecule/default/tests/test_default.py
@@ -7,5 +7,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
+def _get_sosreport_package_name(distribution):
+    return {
+        "ubuntu": "sosreport",
+        "centos": "sos"
+    }.get(distribution)
+
+
 def test_sos_report_is_installed(host):
-    assert host.package('sos').is_installed
+    package_name = _get_sosreport_package_name(host.system_info.distribution)
+    package = host.package(package_name)
+
+    assert package.is_installed
