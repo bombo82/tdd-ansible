0b13cdf5826027f114285ab54a79b3adab1a28bd
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2019-08-27 17:12:47 +0200

    test and ensure that httpd is running and enabled

--- apache-systemd/molecule/default/tests/test_default.py
+++ apache-systemd/molecule/default/tests/test_default.py
@@ -9,3 +9,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_apache2_package(host):
     assert host.package('httpd').is_installed
+
+
+def test_apache_is_running_and_enabled(host):
+    service = host.service('httpd')
+
+    assert service.is_running
+    assert service.is_enabled
