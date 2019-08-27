75c3f0e7523ad0ba5dfab2983b571154821a4d85
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2019-08-27 15:54:52 +0200

    test and ensure that httpd is installed

--- apache-systemd/molecule/default/tests/test_default.py
+++ apache-systemd/molecule/default/tests/test_default.py
@@ -7,9 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_apache2_package(host):
+    assert host.package('httpd').is_installed
