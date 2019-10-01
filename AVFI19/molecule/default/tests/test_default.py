7e1fb1d83d5ab8578b8f1b0b2b1efaee2bbcdc62
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2019-10-01 08:47:58 +0200

    test 'sos_report' is installed
    Signed-off-by: Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>

--- AVFI19/molecule/default/tests/test_default.py
+++ AVFI19/molecule/default/tests/test_default.py
@@ -13,3 +13,17 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def _get_sosreport_package_name(distribution):
+    return {
+        "ubuntu": "sosreport",
+        "centos": "sos"
+    }.get(distribution)
+
+
+def test_sos_report_is_installed(host):
+    package_name = _get_sosreport_package_name(host.system_info.distribution)
+    package = host.package(package_name)
+
+    assert package.is_installed
